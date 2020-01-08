3e1638c8c7b67cbe616871ff76a1699f30fd3365
Cole <digitaldesign.nicole@gmail.com>
2020-01-07 17:56:34 -0800

    Deleted unneccesary lines from default project. Added separate dart files for creating StrainCard widgets.

---
+++ app01_12amconcepts/lib/MainPage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'StrainCard.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StrainCard(),
+    );
+  }
+}
---
+++ app01_12amconcepts/lib/StrainCard.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class StrainCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(); //change this widget to whatever you want for the outermost widget
+  }
+}
