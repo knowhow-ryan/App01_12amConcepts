06e400ccb5fc74d7f3fe0b2f419701d895b88e04
Cole <digitaldesign.nicole@gmail.com>
2020-01-08 09:09:44 -0800

    Cole Playing with layout 1/8/2020

--- app01_12amconcepts/lib/StrainCard.dart
+++ app01_12amconcepts/lib/StrainCard.dart
@@ -3,6 +3,37 @@ import 'package:flutter/material.dart';
 class StrainCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(); //change this widget to whatever you want for the outermost widget
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+        Stack(children: <Widget>[
+          Container(
+            width:150,
+            height:150,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(width: 2.0, color: Colors.red),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(
+                  "http://justcole.design/wp-content/uploads/2019/12/weed@0.25x.png"
+                  )))),
+          Positioned(
+            left:5,
+            bottom:5,
+            child: Container(
+            padding: EdgeInsets.all(10.0),
+            
+            decoration: BoxDecoration(
+              shape:BoxShape.circle,
+              color: Colors.white,
+              border: Border.all(width:2.0, color: Colors.black),),
+              child: Text("01"),
+            )
+          )
+        ],
+      )
+        ] 
+    ); // outermost widget
   }
 }
