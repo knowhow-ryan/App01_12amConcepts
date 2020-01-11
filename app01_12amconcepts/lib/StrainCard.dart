929cebb8900139f3f1e7205a8f832fcfed08106c
Cole <digitaldesign.nicole@gmail.com>
2020-01-11 10:16:10 -0800

    Cole syncing somewhat final card layout

--- app01_12amconcepts/lib/StrainCard.dart
+++ app01_12amconcepts/lib/StrainCard.dart
@@ -3,37 +3,99 @@ import 'package:flutter/material.dart';
 class StrainCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+    return Container(
+      
+      padding: EdgeInsets.fromLTRB(0, 50, 0, 50),
+      //height: MediaQuery.of(context).copyWith().size.height * .45,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        border: Border.all(width: 2.0, color:Colors.green),
+        color: Colors.black87,
+      ),
+      child: Row(
         children: <Widget>[
-        Stack(children: <Widget>[
-          Container(
-            width:150,
-            height:150,
+        Stack(children: <Widget>[ 
+          Container( //Card Container
+            padding: EdgeInsets.only(top:MediaQuery.of(context).copyWith().size.height * .03,),
+            width: MediaQuery.of(context).copyWith().size.width * .95,
+            child: 
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Container( //Card
+                  height:MediaQuery.of(context).copyWith().size.width * .35,
+                  decoration: BoxDecoration(
+                    color: Colors.black87,
+                    gradient: LinearGradient(
+                      colors: [Colors.grey, Colors.grey],
+                      begin: Alignment.bottomRight,
+                      end: Alignment.topLeft,
+                      
+                      ),
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(12.0),
+                    boxShadow:[ BoxShadow(
+                      color: Colors.black12,
+                      blurRadius:10,
+                      offset: Offset(0.0, 10),
+                    ),]
+                  ),
+                  margin: EdgeInsets.only(left:MediaQuery.of(context).copyWith().size.width * .25,),
+                  padding: EdgeInsets.only(left:MediaQuery.of(context).copyWith().size.width * .125,),
+                  child: Column(children: <Widget>[
+                    
+                     ListTile(
+                    title:Text("Gorilla Glue #12"),
+                    subtitle: Text("from K9 Kronic"),
+                  ),
+                  
+                  Row(children: <Widget>[
+                    Text("%%THC"),
+                    SizedBox(width:MediaQuery.of(context).copyWith().size.width * .025,),
+                    Text("%%CBD"),
+                    Text("DATE"),
+                  ],)
+                  ],)
+                ),
+              ],
+            ),
+          ),
+          Container( //Strain Image - Need to decide default image
+            margin: EdgeInsets.fromLTRB(12, 28, 12, 12),
+            width:MediaQuery.of(context).copyWith().size.width * .32,
+            height:MediaQuery.of(context).copyWith().size.width * .32,
             decoration: BoxDecoration(
               shape: BoxShape.circle,
+              color: Colors.white,
               border: Border.all(width: 2.0, color: Colors.red),
               image: DecorationImage(
                 fit: BoxFit.cover,
                 image: NetworkImage(
                   "http://justcole.design/wp-content/uploads/2019/12/weed@0.25x.png"
                   )))),
-          Positioned(
+          Positioned( //Rating Number Container
             left:5,
             bottom:5,
             child: Container(
-            padding: EdgeInsets.all(10.0),
+            padding: EdgeInsets.all(13.0),
             
             decoration: BoxDecoration(
               shape:BoxShape.circle,
-              color: Colors.white,
+              color: Colors.black,
               border: Border.all(width:2.0, color: Colors.black),),
-              child: Text("01"),
-            )
-          )
+              child: 
+                Text("01",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                )),
+            ),
+          ),
         ],
-      )
-        ] 
-    ); // outermost widget
+      ), // outermost widget
+        
+        ],),
+    );
   }
 }
