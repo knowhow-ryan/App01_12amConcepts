2d1220f4ba884c7d7d384d7074c4b776f98e7ff7
Cole <digitaldesign.nicole@gmail.com>
2020-01-27 15:23:14 -0800

    Trying to add screen change with floating action

--- app01_12amconcepts/lib/MainPage.dart
+++ app01_12amconcepts/lib/MainPage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-// import 'StrainPage.dart';
-// import 'StrainCard.dart';
+import 'StrainPage.dart';
+import 'StrainCard.dart';
 import 'NewExperiencePage.dart';
 // import 'ExperienceInputStep2.dart';
 
@@ -10,11 +10,36 @@ class MainPage extends StatelessWidget {
  
     
     return Scaffold(
-     
+     floatingActionButton: FloatingActionButton(
+      onPressed: () {
+            Navigator.of(context).push(_createRoute());
+          },
+          
+      child: Icon(Icons.done),
+      backgroundColor: Color(0xFF8BD3A8),
+    ),
     
       body: Container(
         
         child: NewExperiencePage(),
     ));
   }
+}
+
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => NewExperiencePage(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = Offset(0.0, 1.0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
 }
--- app01_12amconcepts/lib/NewExperiencePage.dart
+++ app01_12amconcepts/lib/NewExperiencePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'StrainPage.dart';
 
 
 class NewExperiencePage extends StatefulWidget {
@@ -10,28 +11,250 @@ class NewExperiencePage extends StatefulWidget {
   NewExperiencePageState createState() => NewExperiencePageState();
 }
 
+// class MyStyle extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData(
+//         brightness: Brightness.dark,
+//         primaryColor: Color(0xFFb2d3bf),
+//         accentColor: Colors.white,
+        
+//         textTheme: TextTheme(
+          
+//           headline: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+//           title: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+//           body1: TextStyle(fontSize: 18,),
+//           body2: TextStyle(fontSize: 15),
+//         )
+
+//       )
+      
+//     );
+//   }}
+
+// class MyStyle {
+//   static TextStyle mytitle(BuildContext context) {
+//     return Theme.of(context).textTheme.title.copyWith(fontSize: 20);
+//   }
+// }
+
 class NewExperiencePageState extends State<NewExperiencePage> {
   //
   int currentStep = 0;
   List<Step> steps = [
     Step(
       title: Text('What Strain?',
+      // style: MyStyle.mytitle(context),
       style: TextStyle(
         color: Colors.white,
         fontSize: 18,
         fontStyle: FontStyle.italic,
+      )
+      ),
+      content: 
+      Column(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.white54,
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+              ),
+            child: TextField(
+             
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(15),
+                
+                  hintText: "strain name",
+                  hintStyle: TextStyle(fontSize: 15),
+              )
+            ),
+          ),
+          Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadiusDirectional.circular(50),
+                        color: Colors.white70,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(12,8,12,8),
+                        child: Center(
+                          child: Text("Jedi Killer",
+                              style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 13,
+                              )),
+                        ),
+                      )),
+                ),
+                Padding(
+                      padding: const EdgeInsets.only(left:4,right:4,),
+                      child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadiusDirectional.circular(50),
+                            color: Colors.white70,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(12,8,12,8),
+                            child: Center(
+                              child: Text("Hindu Kush",
+                                  style: TextStyle(
+                                    color: Colors.black54,
+                                    fontSize: 13,
+                                  )),
+                            ),
+                          )),
+                    ),
+              ],
+            ),
+        ],
+      ),
+      isActive: true,
+    ),
+    Step(
+      title: Text('Where at?',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 18,
+        fontStyle: FontStyle.italic,
+      )
+      ),
+      content: Column(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.white54,
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+              ),
+            child: TextField(
+             
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(15),
+                
+                  hintText: "Where at?",
+                  hintStyle: TextStyle(fontSize: 15),
+              )
+            ),
+          ),
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadiusDirectional.circular(50),
+                        color: Colors.white70,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(12,8,12,8),
+                        child: Center(
+                          child: Text("home",
+                              style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 13,
+                              )),
+                        ),
+                      )),
+                ),
+                Padding(
+                      padding: const EdgeInsets.only(left:4,right:4,),
+                      child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadiusDirectional.circular(50),
+                            color: Colors.white70,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(12,8,12,8),
+                            child: Center(
+                              child: Text("apartment",
+                                  style: TextStyle(
+                                    color: Colors.black54,
+                                    fontSize: 13,
+                                  )),
+                            ),
+                          )),
+                    ),
+              ],
+            ),
+
+        ],
+      ),
+      isActive: true,
+    
+    ),
+    
+    Step(
+      title: Text('How?',
+       style: TextStyle(
+        color: Colors.white,
+        fontSize: 18,
+        fontStyle: FontStyle.italic,
       )),
-      content: Text('Hello!'),
-      isActive: true,
-    ),
-    Step(
-      title: Text('Where at?'),
-      content: Text('World!'),
-      isActive: true,
-    ),
-    Step(
-      title: Text('How?'),
-      content: Text('Hello World!'),
+      content: Column(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.white54,
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+              ),
+            child: TextField(
+             
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(15),
+                
+                  hintText: "how so",
+                  hintStyle: TextStyle(fontSize: 15),
+              )
+            ),
+          ),
+          Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadiusDirectional.circular(50),
+                        color: Colors.white70,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(12,8,12,8),
+                        child: Center(
+                          child: Text("bong",
+                              style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 13,
+                              )),
+                        ),
+                      )),
+                ),
+                Padding(
+                      padding: const EdgeInsets.only(left:4,right:4,),
+                      child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadiusDirectional.circular(50),
+                            color: Colors.white70,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(12,8,12,8),
+                            child: Center(
+                              child: Text("pipe",
+                                  style: TextStyle(
+                                    color: Colors.black54,
+                                    fontSize: 13,
+                                  )),
+                            ),
+                          )),
+                    ),
+              ],
+            ),
+        ],
+      ),
       state: StepState.complete,
       isActive: true,
     ),
@@ -49,7 +272,7 @@ class NewExperiencePageState extends State<NewExperiencePage> {
               gradient: LinearGradient(
                 end: FractionalOffset.topCenter,
                 begin: FractionalOffset.bottomCenter,
-                stops: [.6, .9,],
+                stops: [.05, .45,],
                 colors: [Color(0xFFDDDDDD), Colors.black87,],
               ),
               ),
@@ -62,8 +285,8 @@ class NewExperiencePageState extends State<NewExperiencePage> {
             ),
           Theme(
             data: ThemeData(
-      primaryColor: Color(0xFFb2d3bf),
-      secondaryHeaderColor: Colors.white,
+      primaryColor: Color(0xFF51B579),
+      
     ),
             child: Stepper(
               currentStep: this.currentStep,
